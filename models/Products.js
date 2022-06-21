5c27a83de9a45c3114166d21a1abdee044f87b17
Tran Khai Hoang <hoangbo74@gmail.com>
2022-06-21 10:34:06 +0700

    fix province selecter (remove localStorage)

--- models/Products.js
+++ models/Products.js
@@ -4,18 +4,18 @@ var slug = require('mongoose-slug-generator');
 mongoose.plugin(slug);
 
 const Products = new Schema({
-    product_name: {type: String, required: true, unique: true},
-    subtitle: {type: String, required: true},
-    product_img: {type: String, required: true},
-    price: {type: Number},
-    showroom: {type: String, required: true},
-    represent: {type: String, required: true},
-    address: {type: String, required: true},
-    phone: {type: String, required: true},
-    slug: {type: String, slug: 'product_name'}
+    product_name: { type: String, required: true, unique: true },
+    subtitle: { type: String, required: true },
+    product_img: { type: String, required: true },
+    price: { type: Number, default: 0 },
+    showroom: { type: String, required: true },
+    represent: { type: String, required: true },
+    address: { type: String, required: true }, 
+    phone: { type: String, required: true },
+    slug: { type: String, slug: 'product_name' }
 },
-{
-    timestamps: true
-})
+    {
+        timestamps: true
+    })
 
 module.exports = mongoose.model('Products', Products);
--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -71,6 +71,7 @@ function selectDistrict(calc_shipping_provinces, calc_shipping_district, billing
         })
     })
     //]]>
+    localStorage.clear()
 }
 
 selectDistrict("calc_shipping_provinces", "calc_shipping_district", "billing_address_1", "billing_address_2", "district_local", "address_1_saved", "address_2_saved")
@@ -115,3 +116,6 @@ $(document).ready(function () {
         }
     });
 });
+
+
+var c = ["An Giang", "Bà Rịa - Vũng Tàu", "Bạc Liêu", "Bắc Kạn", "Bắc Giang", "Bắc Ninh", "Bến Tre", "Bình Dương", "Bình Định", "Bình Phước", "Bình Thuận", "Cà Mau", "Cao Bằng", "Cần Thơ", "Đà Nẵng", "Đắk Lắk", "Đắk Nông", "Đồng Nai", "Đồng Tháp", "Điện Biên", "Gia Lai", "Hà Giang", "Hà Nam", "Hà Nội", "Hà Tĩnh", "Hải Dương", "Hải Phòng", "Hòa Bình", "Hậu Giang", "Hưng Yên", "Thành phố Hồ Chí Minh", "Khánh Hòa", "Kiên Giang", "Kon Tum", "Lai Châu", "Lào Cai", "Lạng Sơn", "Lâm Đồng", "Long An", "Nam Định", "Nghệ An", "Ninh Bình", "Ninh Thuận", "Phú Thọ", "Phú Yên", "Quảng Bình", "Quảng Nam", "Quảng Ngãi", "Quảng Ninh", "Quảng Trị", "Sóc Trăng", "Sơn La", "Tây Ninh", "Thái Bình", "Thái Nguyên", "Thanh Hóa", "Thừa Thiên - Huế", "Tiền Giang", "Trà Vinh", "Tuyên Quang", "Vĩnh Long", "Vĩnh Phúc", "Yên Bái"]
